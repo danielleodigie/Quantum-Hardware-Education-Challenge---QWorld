c9c901aafdccb6132cd9d36d58f6b0525cc2be8b
danielleodigie <odigiedanielle@gmail.com>
2022-08-12 20:26:47 -0400

    adding nav and footer

---
+++ qnychackathon/components/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="footer">
+        <div id="foot">
+        <a href="https://github.com/danielleodigie/Quantum-Hardware-Education-Challenge---QWorld" target="_blank">Github Repo</a>
+        <p>Quantum is for Everyone</p>
+        </div>
+    </div>
+</template>
+
+<style>
+    #footer{
+        margin-top: auto;
+        text-align:center;
+        margin: auto;
+        font-family: "Goldleaf";
+        padding: 10px;
+    }
+
+    #foot{ 
+       text-align: center;
+    }
+
+    #footer a, a:visited {
+        transition: 200ms;
+        color: white;
+        padding: 8px 30px;
+        border: 1px solid;
+        border-radius: 10px;
+        background-color: #5a4230;
+    }
+
+    #footer a:hover{
+        transition: 200ms;
+        background-color: #3b2a1d;
+    }
+
+    p{
+        font-size: 12px;
+        
+    }
+</style>
---
+++ qnychackathon/components/NavBar.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="nav">
+        <NuxtLink to="/" id="home">QNYC</NuxtLink>
+        <ul id="otherlinks">
+            <NuxtLink to="#">About</NuxtLink>
+            <NuxtLink to="#">Learn</NuxtLink>
+        </ul>
+    </div>
+</template>
+
+<style>
+    @font-face{
+        font-family: "GoldLeaf";
+        src: url("../static/fonts/GoldleafBoldPersonalUseBold-eZ4dO.ttf")
+    }
+
+    @font-face{
+        font-family: "Quartzo";
+        src: url("../static/fonts/QuartzoBold-W9lv.ttf")
+    }
+
+
+    body{
+        margin: 0;
+    }
+    #nav{
+        padding: 20px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+    }
+    #home{
+        float: left;
+        font-family: "Quartzo";
+        font-size: 35px;
+        padding-left: 20px;
+        color: #5a4230;
+    }
+    #otherlinks{
+        float: right;
+    }
+
+    #otherlinks a {
+        padding-right: 80px;
+        font-family: "Goldleaf";
+        font-size: 25px;
+        color: black;
+    }
+
+    a{
+        text-decoration:none;
+
+    }
+</style>
