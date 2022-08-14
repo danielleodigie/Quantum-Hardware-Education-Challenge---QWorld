fb473c6410207199a7fafd66969c8005a490d440
danielleodigie <odigiedanielle@gmail.com>
2022-08-14 10:37:25 -0400

    adding module nav

---
+++ qnychackathon/components/ModuleNav.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="modulenav">
+        <ul>
+            <li v-for="(item, index) in navitems">
+                <span><a :href="navlinks[index]">{{item}}</a></span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default{
+        props: {
+            navitems: Array,
+            navlinks: Array
+        }
+    }
+</script>
+
+<style>
+    #modulenav li{
+
+        text-decoration: none;
+        list-style:circle;
+        color: white;
+        font-size: 30px;
+    }
+    #modulenav li span{
+        font-size: 20px;
+    }
+    #modulenav a, #modulenav a:visited{
+        color: white;
+        font-family: "Goldleaf";
+        font-size: 30px;
+    }
+
+    #modulenav{
+        width: 210px;
+        height: 100%;
+        background-color: #6f5947;
+        border-radius: 0px 15px 15px 0px;
+        display: flex;
+    }
+
+    #modulenav ul{
+        margin:auto;
+        padding:15px;
+    }
+</style>
