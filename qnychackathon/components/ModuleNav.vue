a4e77062c9e7dbd9189c7192b3e11d2f7325ea27
danielleodigie <odigiedanielle@gmail.com>
2022-08-19 23:11:08 -0400

    adding lecture page NOT DONE

--- qnychackathon/components/ModuleNav.vue
+++ qnychackathon/components/ModuleNav.vue
@@ -1,41 +1,76 @@
 <template>
+    <aside :class="`${is_expanded ? 'is-expanded' : ''}`">
+    <img @click="toggle()" :src="navimg" id="navimg"/>
     <div id="modulenav">
-        <ul>
+        <ul id="modulenavlist">
             <li v-for="(item, index) in navitems">
                 <span><a :href="navlinks[index]">{{item}}</a></span>
             </li>
         </ul>
     </div>
+    </aside>
 </template>
 
 <script>
+    import {ref} from 'vue'
     export default{
-        props: {
-            navitems: Array,
-            navlinks: Array
+        
+        data(){
+            return {
+                navitems: ["Classical and Quantum Bits"],
+                navlinks: ["/learn/lecturepage1"],
+                is_expanded: false,
+                navimg: "../images/plus.png"
+            }
+        },
+        methods: {
+            toggle() {
+                this.is_expanded = !this.is_expanded
+                if(!this.is_expanded){
+                    document.getElementById("modulenavlist").style.visibility = "hidden";
+                    this.navimg= "../images/plus.png"
+                }
+                else{
+                    document.getElementById("modulenavlist").style.visibility = "visible";
+                    this.navimg= "../images/minus.png"
+                }
+                
+            }
         }
     }
 </script>
 
 <style>
+    aside{
+        transition: 0.2s ease-in-out;
+        width: 2em; 
+        background-color: #6f5947;
+        border-radius: 0px 10px 10px;
+        position: absolute;
+        top: 120px;
+    }
+
+    .is-expanded{
+        width:200px;
+    }
     #modulenav li{
 
         text-decoration: none;
         list-style:circle;
         color: white;
-        font-size: 30px;
+        font-size: 15px;
     }
     #modulenav li span{
-        font-size: 20px;
+        font-size: 15px;
     }
     #modulenav a, #modulenav a:visited{
         color: white;
         font-family: "Goldleaf";
-        font-size: 30px;
+        font-size: 20px;
     }
 
     #modulenav{
-        width: 210px;
+        overflow-x: hidden;
         height: 100%;
         background-color: #6f5947;
         border-radius: 0px 15px 15px 0px;
@@ -45,5 +80,13 @@
     #modulenav ul{
         margin:auto;
         padding:15px;
+    }
+
+    #navimg{
+        transition: 0.1s;
+        height: 20px;
+        margin:auto;
+        padding-left: 5px;
+        padding-top: 10px;
     }
 </style>
