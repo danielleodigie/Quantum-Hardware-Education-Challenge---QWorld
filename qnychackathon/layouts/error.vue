89b392c9388657373c47e6feaf34864474dc5753
danielleodigie <odigiedanielle@gmail.com>
2022-08-23 19:01:15 -0400

    adding error page

---
+++ qnychackathon/layouts/error.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="error">
+    <h1>We've seemed to have lost our qubits!</h1>
+    <p>Let's go <NuxtLink to="/">home</NuxtLink>!</p>
+    <img src="images/S_Cat 2.gif" alt="superposition cat">
+    </div>
+</template>
+
+<style>
+    #error{
+        margin: auto;
+        text-align: center;
+        overflow-x: hidden;
+    }
+
+    #error p{
+        font-size: 30px;
+    }
+
+    #error a{
+        color: #5a4230;
+    }
+
+    #error img{
+        height: 500px;
+    }
+</style>
